968ef0a4bbe22ac0281a8ecf128a0d0a6a7b45de
PraveenM24 <praveen050800@gmail.com>
2021-05-24 10:31:52 +0530

    Initial commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,9 @@
-const form = document.querySelector('form'); // grabbing an element on the page
+const form = document.querySelector('form'); 
 const errorElement = document.querySelector('.error-message');
 const loadingElement = document.querySelector('.loading');
 const mewsElement = document.querySelector('.mews');
 const loadMoreElement = document.querySelector('#loadMore');
 const API_URL = 'https://tpgitminiblog.herokuapp.com/posts';
-
-/*const { Credential } = require('./login.js');
-console.log(Credential);*/
 
 let skip = 0;
 let limit = 5;
@@ -29,7 +26,6 @@ form.addEventListener('submit', (event) => {
     const formData = new FormData(form);
     const name = formData.get('name');
     const content = formData.get('content');
-
     var created = new Date();
 
     if (name.trim() && content.trim()) {
@@ -37,11 +33,16 @@ form.addEventListener('submit', (event) => {
         form.style.display = 'none';
         loadingElement.style.display = '';
 
-        const mew = {
+        var mew = {
             name,
             content,
             created
-        };
+        }
+
+        const fdata = new FormData();
+        fdata.append('name', name);
+        fdata.append('content', content);
+        fdata.append('created', created);
 
         fetch(API_URL, {
             method: 'POST',
@@ -73,11 +74,12 @@ form.addEventListener('submit', (event) => {
     } else {
         errorElement.textContent = 'Content is missing..';
         errorElement.style.display = '';
-        setTimeout(function() {
+        setTimeout(function () {
             errorElement.style.display = 'none';
         }, 3000);
     }
 });
+
 
 function loadMore() {
     skip += limit;
@@ -85,45 +87,78 @@ function loadMore() {
 }
 
 function listAllMews(reset = true) {
+    var combined_data = []
     loading = true;
     if (reset) {
         mewsElement.innerHTML = '';
         skip = 0;
         finished = false;
     }
-    var table = document.getElementById('table')
-    fetch('https://tpgitminiblog.herokuapp.com/posts')
-        .then(function(response) {
+
+    fetch('https://tpgitminiblog-api.herokuapp.com/api')
+        .then(function (response) {
             return response.json();
         })
-        .then(function(data) {
+        .then(function (data) {
+            combined_data.push(data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+    fetch('https://tpgitminiblog.herokuapp.com/posts')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            combined_data.push(data);
             length = data.length;
-            for (var i = length - 1; i >= 0; i--) {
+            for (var i = 0; i < length; i++) {
+                newDate = new Date(data[i].created).toString()
+                data[i].created = newDate
+            }
+
+            var final = combined_data[0].concat(combined_data[1])
+            final.sort((a, b) => {
+                let da = new Date(a.created),
+                    db = new Date(b.created);
+                return da - db;
+            });
+
+            length_final = final.length;
+            for (var i = length_final-1; i >= 0; i--) {
 
                 const div = document.createElement('div');
+                div.setAttribute('class', 'chatelement');
 
                 const header = document.createElement('a');
                 header.setAttribute('href', './profile.html');
                 header.setAttribute('target', '_blank');
                 header.setAttribute('class', 'name');
-                header.innerHTML = data[i].name
-                    //header.textContent = data[i].name;
+                header.innerHTML = final[i].name
+                div.appendChild(header);
+                
 
                 const contents = document.createElement('p');
-                contents.textContent = data[i].content;
+                contents.textContent = final[i].content;
+                div.appendChild(contents);
+
+                if (typeof(final[i].image) != "undefined") {
+                    const image = document.createElement('IMG');
+                    var url = 'https://tpgitminiblog-api.herokuapp.com/' + final[i].image;
+                    image.setAttribute('src', url);
+                    image.setAttribute("width", "100%");
+                    image.setAttribute("height", "fit-content");
+                    div.appendChild(image);
+                }
 
                 const date = document.createElement('small');
-                date.textContent = new Date(data[i].created);
-
-                div.appendChild(header);
-                div.appendChild(contents);
+                date.textContent = new Date(final[i].created);
                 div.appendChild(date);
-
                 mewsElement.appendChild(div);
-
             }
         })
-        .catch(function(error) {
+        .catch(function (error) {
             console.log(error);
         });
 }
